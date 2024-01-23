8df63d95b71e09499223667abed6df9da53b5305
Carl Barrdahl <carlbarrdahl@gmail.com>
2024-01-23 12:34:39 +0100

    Update landing page

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,12 @@ import Image from "next/image";
 import { Button } from "./_components/ui/Button";
 import { ComponentProps } from "react";
 import Link from "next/link";
+import { createComponent } from "./_components/ui";
+import { tv } from "tailwind-variants";
+import { QR } from "./_components/qr";
+import { headers } from "next/headers";
 
-const demoURL =
+const demoID =
   "H4sIAAAAAAAAA2WQMU7DQBBFrzKa2oWdEKS4A0sRB6CLKCbrMbJi76xmdwsS%2BQZQItFR09FwAm7CCTgCIySihGiKlea%2F%2F0baPXoaGWtsZPcATQ7QUPf5hgX6PG5YLalm82VZVpezpW2DSptdiliv93%2FVr5dnaKTrmA1oOTrtQ%2BrFW3TFKkE5Rm7BYO0deTGKRsk%2BYT1fTMXB8%2F369AG3TGeWG9YNDZB%2Bo0O1PK0%2BwkpznyDSQO0RVy3%2B667J2xRAIQxcgCj5e3tVKCZWcOLE5wTdQFuOeHrl3f5ny0f6izM9TnfTDxIv97BXAQAA";
 export default function Home() {
   return (
@@ -13,10 +17,13 @@ export default function Home() {
           swisher
         </h1>
 
-        <p className="leading-8 text-lg tracking-wide">
+        <P className={"text-xl"}>
           Swisher är en mikroapp för att skapa menyer där betalning sker genom
           Swish.
-        </p>
+        </P>
+        <P>
+          Du kan skapa en shop och får sedan en QR-kod dina kunder kan skanna.
+        </P>
 
         <div className="flex flex-col gap-1">
           <Button as="a" href={"/shop/new"} size="lg" variant="primary">
@@ -27,15 +34,19 @@ export default function Home() {
         <div className="">
           <h3 className="text-sm tracking-wider text-gray-600">Exempel</h3>
           <div className="flex gap-1">
-            <Button className="w-full" as="a" href={`/shop/${demoURL}`}>
+            <Button className="w-full" as="a" href={`/shop/${demoID}`}>
               Café
             </Button>
             <Button className="w-full">Marknad</Button>
           </div>
         </div>
         <div className="py-16">
-          <div className="flex justify-center">
-            <div className="border-2 p-4 border-primary-400 rounded-2xl shadow-lg">
+          <div className="flex flex-col items-center gap-8 justify-center">
+            <a
+              href={`/shop/${demoID}`}
+              target="_blank"
+              className="border-2 p-4 border-primary-200 rounded-2xl shadow hover:border-primary-400 hover:shadow-xl hover:-translate-y-1 transition-all"
+            >
               {/* <div className="bg-gradient-to-r p-0.5 rounded-2xl from-cyan-400 via-green-400  to-pink-500"> */}
               <Image
                 className="rounded-xl"
@@ -44,7 +55,11 @@ export default function Home() {
                 height={425}
                 src={`/swisher.png`}
               />
+            </a>
+            <div className="text-sm text-gray-700 tracking-widest">
+              Eller skanna QR-koden
             </div>
+            <SwishQR encoded={demoID} />
           </div>
         </div>
         <footer className="pt-8 text-xs flex flex-col items-center">
@@ -66,6 +81,15 @@ export default function Home() {
   );
 }
 
+function SwishQR({ encoded = "" }) {
+  const host = headers().get("host");
+  const shopURL = `https://${host}/shop/${encoded}`;
+
+  return <QR className="rounded" size={300} value={shopURL} />;
+}
+
 function A(props: ComponentProps<typeof Link>) {
   return <Link className="underline underline-offset-2" {...props} />;
 }
+
+const P = createComponent("p", tv({ base: "leading-8 tracking-wide" }));
