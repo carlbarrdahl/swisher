a8e085be13ae815be256acb3714a2775760ec6f8
Carl Barrdahl <carlbarrdahl@gmail.com>
2024-01-23 12:50:48 +0100

    Add how it works section on landing page

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ import { tv } from "tailwind-variants";
 import { QR } from "./_components/qr";
 import { headers } from "next/headers";
 
+const repoURL = "https://github.com/carlbarrdahl/swisher";
 const demoID =
   "H4sIAAAAAAAAA2WQMU7DQBBFrzKa2oWdEKS4A0sRB6CLKCbrMbJi76xmdwsS%2BQZQItFR09FwAm7CCTgCIySihGiKlea%2F%2F0baPXoaGWtsZPcATQ7QUPf5hgX6PG5YLalm82VZVpezpW2DSptdiliv93%2FVr5dnaKTrmA1oOTrtQ%2BrFW3TFKkE5Rm7BYO0deTGKRsk%2BYT1fTMXB8%2F369AG3TGeWG9YNDZB%2Bo0O1PK0%2BwkpznyDSQO0RVy3%2B667J2xRAIQxcgCj5e3tVKCZWcOLE5wTdQFuOeHrl3f5ny0f6izM9TnfTDxIv97BXAQAA";
 export default function Home() {
@@ -23,8 +24,34 @@ export default function Home() {
         </P>
         <P>
           Du kan skapa en shop och får sedan en QR-kod dina kunder kan skanna.
+          Ingen registrering krävs och ditt telefonnummer sparas inte i någon
+          databas.
         </P>
+        <details className="text-sm">
+          <summary className="p-2 cursor-pointer text-right">
+            Hur funkar det?
+          </summary>
 
+          <ol className="list-disc list-inside space-y-4 mb-8">
+            <li>
+              Shoppens information komprimeras till en text-sträng som får plats
+              i en QR-kod.
+            </li>
+            <li>
+              När koden skannas omvandlas text-strängen till ett objekt som
+              innehåller Shoppens information och produkter.
+            </li>
+            <li>
+              Din Shop sparas också lokalt på din mobil vilket gör att du kan
+              redigera den i efterhand.
+            </li>
+            <li>
+              <A href={repoURL} target="_blank">
+                Läs mer om de tekniska detailjerna och sidans källkod.
+              </A>
+            </li>
+          </ol>
+        </details>
         <div className="flex flex-col gap-1">
           <Button as="a" href={"/shop/new"} size="lg" variant="primary">
             Skapa en ny Shop
@@ -47,7 +74,6 @@ export default function Home() {
               target="_blank"
               className="border-2 p-4 border-primary-200 rounded-2xl shadow hover:border-primary-400 hover:shadow-xl hover:-translate-y-1 transition-all"
             >
-              {/* <div className="bg-gradient-to-r p-0.5 rounded-2xl from-cyan-400 via-green-400  to-pink-500"> */}
               <Image
                 className="rounded-xl"
                 alt="Swisher screenshot"
@@ -65,7 +91,7 @@ export default function Home() {
         <footer className="pt-8 text-xs flex flex-col items-center">
           <div>
             Swisher är gratis &{" "}
-            <A target="_blank" href="https://github.com/carlbarrdahl/swisher">
+            <A target="_blank" href={repoURL}>
               öppen källkod
             </A>
           </div>
